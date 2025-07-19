37bf8d9342022169f3fef2738fbf2e5ce2d1ffa6
Md-Afjal-1 <mdafjal123558888@gmail.com>
2025-07-20 00:35:14 +0530

    Updated public folder structure and email API endpoint

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -34,3 +34,11 @@ const backToTopBtn = document.getElementById('backToTopBtn');
 
     // animation on scroll
     
+// loder
+window.addEventListener('load', function () {
+  const preloader = document.getElementById('preloader');
+  preloader.style.opacity = '0';
+  setTimeout(() => {
+    preloader.style.display = 'none';
+  }, 500);
+});
