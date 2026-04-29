337da64608f35f3919df46ca75216ac1ce3d37f1
Md-Afjal-1 <mdafjal123558888@gmail.com>
2026-04-29 11:12:28 +0530

    portfolio update

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 const express = require("express");
 const nodemailer = require("nodemailer");
 const cors = require("cors");
@@ -12,6 +13,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 
 // Static files serve karega (public folder)
 app.use(express.static(path.join(__dirname, "public")));
